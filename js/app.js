60b9c1f17e46888a80ed3626ac39e97249a6cb0c
ukpono-j <ukponoakpan270@gmail.com>
2022-03-22 15:43:03 +0100

    make it better

--- js/app.js
+++ js/app.js
@@ -247,18 +247,15 @@ $(() => {
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function() {
-        alert("You have been signed out successfully");
-        $(".data").css("display", "none");
-        $(".g-signin2").css("display", "block");
+        console.log('User signed out.');
     });
 }
