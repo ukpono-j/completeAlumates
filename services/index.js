9f877e7e4f3e46b92b590a0dbe1a99943ce6bb81
Joel Okoromi <okmarq@gmail.com>
2022-03-28 16:01:41 +0100

    cors setup

--- services/index.js
+++ services/index.js
@@ -1,12 +1,18 @@
 function get(url) {
     return $.ajax({
-        url: url
+        url: url,
+        headers: {
+            'Access-Control-Allow-Origin': 'https://alumates111.herokuapp.com'
+        }
     })
 }
 
 function post(url, data) {
     return $.ajax({
         url: url,
+        headers: {
+            'Access-Control-Allow-Origin': 'https://alumates111.herokuapp.com'
+        },
         method: "POST",
         data: data
     })
